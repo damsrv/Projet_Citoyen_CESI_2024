3654f19ad34efa1df1b45cfeed1c3523ca962394
Christopher Semard <christophersemard76@gmail.com>
2024-05-23 11:08:45 +0200

    ADD : footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
-import type {Metadata} from "next";
-import {Outfit} from "next/font/google";
+import type { Metadata } from "next";
+import { Outfit } from "next/font/google";
 import "./globals.css";
-import {cn} from "@/lib/utils";
+import { cn } from "@/lib/utils";
 import Topbar from "@/components/Topbar/Topbar";
-import {ReactNode} from "react";
+import { ReactNode } from "react";
+import Footer from "@/components/Footer/Footer";
 
-const inter = Outfit({subsets: ["latin"], variable: "--font-sans"});
+const inter = Outfit({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
     title: "Create Next App",
@@ -13,18 +14,21 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
+    children,
+}: Readonly<{
     children: ReactNode;
 }>) {
     return (
         <html lang="fr">
-            <body className={cn(
-                "min-h-screen bg-background font-sans antialiased",
-                inter.variable
-            )}>
-            <Topbar/>
-            {children}
+            <body
+                className={cn(
+                    "min-h-screen bg-background font-sans antialiased",
+                    inter.variable
+                )}
+            >
+                <Topbar />
+                {children}
+                <Footer />
             </body>
         </html>
     );
