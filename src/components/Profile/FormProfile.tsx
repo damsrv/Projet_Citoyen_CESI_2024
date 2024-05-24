8c70435b7b3980b8e844da8a6b7c6f3132df4f05
Christopher Semard <christophersemard76@gmail.com>
2024-05-24 09:23:05 +0200

    WIP : layout

---
+++ src/components/Profile/FormProfile.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Input } from "@/components/ui/input";
+
+const FormProfile = () => {
+    return (
+        <form action="">
+            <Input type="email" placeholder="Email" />
+        </form>
+    );
+};
+
+export default FormProfile;
---
+++ src/components/ProfileNav/ProfileNav.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProfileNav = () => {
+    return (
+        <div className="bg-white border h-64 w-64 rounded-lg p-5">
+            ProfileNav
+        </div>
+    );
+};
+
+export default ProfileNav;
