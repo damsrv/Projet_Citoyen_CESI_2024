6018af483559fc0d46aee87af9f6ecf27b051a0a
Damien COTE <damien_76_@hotmail.fr>
2024-06-15 20:01:31 +0200

    feat(PCitoyen): Ajout route api pour messages et rooms.

---
+++ src/app/api/messages/[id]/route.ts
@@ -0,0 +1,72 @@
+import prisma from "@/lib/prisma";
+import { PrismaClient, Prisma } from '@prisma/client'
+import { NextResponse } from "next/server";
+import PrismaClientKnownRequestError = Prisma.PrismaClientKnownRequestError;
+
+interface Params {
+    params: {
+        id: string;
+    }
+}
+
+export async function PUT(req: Request, params: Params) { 
+
+    const messageId = parseInt(params.params.id);
+    const { data } = await req.json();
+    const { content } = data;
+    
+    try {
+        const deletedUser = await prisma.message.update({
+            where: {
+                id: messageId,
+            },
+            data: { 
+                content,
+                status: 1 
+            },
+        })
+        return NextResponse.json({ status: 204 });
+
+    } catch (e) {
+        console.log(e);
+
+        if (e instanceof Prisma.PrismaClientKnownRequestError && e.code === "P2025") {
+            return NextResponse.json({ error: e, message: "Le Message n'existe pas." }, { status: 404 });
+        } else {
+            return NextResponse.json({
+                error: e,
+                message: "Une erreur est survenue, veuillez réessayer plus tard."
+            }, { status: 500 });
+        }
+    }
+
+}
+
+export async function DELETE(req: Request, params: Params) { 
+
+    const messageId = parseInt(params.params.id);
+    const status  = 2; // deleted status.
+    
+    try {
+        const deletedUser = await prisma.message.update({
+            where: {
+                id: messageId,
+            },
+            data: { status },
+        })
+        return NextResponse.json({ status: 204 });
+
+    } catch (e) {
+        console.log(e);
+
+        if (e instanceof Prisma.PrismaClientKnownRequestError && e.code === "P2025") {
+            return NextResponse.json({ error: e, message: "Le Message n'existe pas." }, { status: 404 });
+        } else {
+            return NextResponse.json({
+                error: e,
+                message: "Une erreur est survenue, veuillez réessayer plus tard."
+            }, { status: 500 });
+        }
+    }
+
+}
