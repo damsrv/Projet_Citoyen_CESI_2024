b3b4f062cda39df0ff22767cc63696abdca8861c
Christopher Semard <christophersemard76@gmail.com>
2024-06-27 15:35:46 +0200

    FIX : nom du mentor

--- src/app/api/offer-student/route.ts
+++ src/app/api/offer-student/route.ts
@@ -1,35 +1,40 @@
 import prisma from "@/lib/prisma";
-import {OfferStudentStatus} from "@/enums/offerStatus";
-import {NextResponse} from "next/server";
-import {PrismaClientValidationError} from "@prisma/client/runtime/library";
+import { OfferStudentStatus } from "@/enums/offerStatus";
+import { NextResponse } from "next/server";
+import { PrismaClientValidationError } from "@prisma/client/runtime/library";
 
 export async function POST(req: Request) {
-    const {data} = await req.json()
-    const {offerId, userId, message} = data;
+    const { data } = await req.json();
+    console.log(data);
+    const { offerId, userId, message } = data;
 
     try {
         const newOfferStudent = await prisma.offerStudent.create({
             data: {
                 message: message,
-                offer: {
-                    connect: {id: offerId}
-                },
-                student: {
-                    connect: {id: userId}
-                },
+                offerId: offerId,
+                studentId: userId,
                 status: OfferStudentStatus.Pending,
-            }
-        })
+            },
+        });
 
-        return NextResponse.json(newOfferStudent, {status: 201})
+        return NextResponse.json(newOfferStudent, { status: 201 });
     } catch (e) {
+        console.log(e);
         if (e instanceof PrismaClientValidationError) {
-            return NextResponse.json({error: e, message: "Erreur de validation prisma"}, {status: 404});
+            return NextResponse.json(
+                { error: e, message: "Erreur de validation prisma" },
+                { status: 404 }
+            );
         } else {
-            return NextResponse.json({
-                error: e,
-                message: "Une erreur est survenue, veuillez réessayer plus tard."
-            }, {status: 500});
+            return NextResponse.json(
+                {
+                    error: e,
+                    message:
+                        "Une erreur est survenue, veuillez réessayer plus tard.",
+                },
+                { status: 500 }
+            );
         }
     }
 }
